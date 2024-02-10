8be3cbe41f3f0890aea2de21a8f65e47004b0cd4
Iot-Health-Br <igl_macedo@hotmail.com>
2024-02-10 09:03:10 -0300

    Código funcionando, estruturado o add de arquivos, apresentação do diretório e o limpar do jlist. Obs: Falta comentar o código

--- src/View/ViewPrint.java
+++ src/View/ViewPrint.java
@@ -18,13 +18,17 @@ public class ViewPrint {
 
     public ViewPrint() {
         controlador = new PrintController();
-        botaoImprimir.setEnabled(false); // Habilitar o botão de adicionar após a seleção do arquivo
+        DefaultListModel<String> modelo = new DefaultListModel<>();
+
+        // Habilitar o botão de adicionar após a seleção do arquivo
+        botaoImprimir.setEnabled(false);
         botaoLimpar.setEnabled(false);
 
 
         botaoAdicionar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Abre a escolha de arquivo no diretorio downloads
                 fileChooser = new JFileChooser("C:/Users/igl_m/Downloads");
                 fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
                 fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("Imagens JPG", "jpg", "jpeg"));
@@ -32,7 +36,9 @@ public class ViewPrint {
                 int returnValue = fileChooser.showOpenDialog(null);
                 if (returnValue == JFileChooser.APPROVE_OPTION) {
                     File selectedFile = fileChooser.getSelectedFile();
-                    botaoImprimir.setEnabled(true); // Habilitar o botão de adicionar após a seleção do arquivo
+
+                    // Habilitar o botão de adicionar após a seleção do arquivo
+                    botaoImprimir.setEnabled(true);
                     botaoLimpar.setEnabled(false);
                     controlador.adicionarArquivo(selectedFile.getAbsolutePath());
                 }
@@ -42,24 +48,24 @@ public class ViewPrint {
         botaoImprimir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                atualizarListaDeImpressao();
                 botaoImprimir.setEnabled(false);
                 botaoLimpar.setEnabled(true);
+
+                modelo.clear();
+                for (String arquivo : controlador.getFilaDeImpressao()) {
+                    modelo.addElement(arquivo);
+                }
+                listaDeImpressao.setModel(modelo);
             }
         });
+
         botaoLimpar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                modelo.clear();
+                botaoImprimir.setEnabled(true);
             }
         });
-    }
-    private void atualizarListaDeImpressao() {
-        DefaultListModel<String> modelo = new DefaultListModel<>();
-        for (String arquivo : controlador.getFilaDeImpressao()) {
-            modelo.addElement(arquivo);
-        }
-        listaDeImpressao.setModel(modelo);
     }
 
     public static void main(String[] args) {
@@ -67,7 +73,7 @@ public class ViewPrint {
             @Override
             public void run() {
                 //new ViewPrint();
-                JFrame frame = new JFrame("Tela de Cadastro");
+                JFrame frame = new JFrame("Tela de Impressão");
                 frame.setContentPane(new ViewPrint().panelMain);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.pack();
